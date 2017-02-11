2ec3111956feb7ab8554f6826158b0466a165600
jacl38 <clarkjack1138@gmail.com>
2017-02-10 20:29:10 -0500

    ☺

--- src/org/usfirst/frc/team237/robot/commands/autonomous/AlignToGear.java
+++ src/org/usfirst/frc/team237/robot/commands/autonomous/AlignToGear.java
@@ -39,7 +39,7 @@ public class AlignToGear extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (onTarget || targetVisible);
+        return (onTarget || !targetVisible);
     }
 
     // Called once after isFinished returns true
